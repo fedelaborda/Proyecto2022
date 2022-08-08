3d063ef4aa0ddf4ffdceb124fe42b3526a23c53f
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-08-08 11:06:06 -0300

    biblioteca actualizada

--- biblioteca.php
+++ biblioteca.php
@@ -5,8 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca - OrtLauncher</title>
+    <link rel="stylesheet" href="estilobiblioteca.css" />
 </head>
 <body>
+    <div id = "buscar">
+        <header>
+            <div class="buscar">
+                <input type="text" placeholder="Buscar..." required>
+            </div> 
+            <div class="btnBuscar">
+                <input type="submit" value="Buscar">
+            </div> 
+        </header>
+    </div> 
     
+    <aside class = "costado">
+        <h2>Juegos Descargados</h2>
+        <ul>
+            <li>
+                Juego 1
+            <li>
+            <li>
+                Juego 2
+            <li>
+            <li>
+                Juego 3
+            <li>
+        <ul>
+    </aside>
+
+
 </body>
 </html>
